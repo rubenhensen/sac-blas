2f94f21b95d22f2a7cbdf5994f01296c0eabe951
Ruben Hensen <ruben.hensen@protonmail.com>
2024-03-16 17:25:21 +0100

    Add partial sasum

--- blas/src/sasum.c
+++ blas/src/sasum.c
@@ -1,7 +1,6 @@
 #include <cblas.h>
 
-void SACsrotg(float *a, float *b, float *c, float *s)
+float SACsasum(const int n, const float *x, const int incx)
 {
-    cblas_srotg(a, b, c, s);
+    return cblas_sasum(n, x, incx);
 }
-

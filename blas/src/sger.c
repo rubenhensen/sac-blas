dab55b43cb7fbf2b1f1e8b7ac4b4ee309a6efcf5
Ruben Hensen <rubenhensen@pop-os.localdomain>
2024-03-22 21:41:58 +0100

    Add sger

---
+++ blas/src/sger.c
@@ -0,0 +1,7 @@
+#include <cblas.h>
+
+void SACsger(const int m, const int n, const float alpha, const float *x, const int incx, const float *y, const int incy, float *a, const int lda)
+{
+    cblas_sger(CblasRowMajor, m, n, alpha, x, incx, y, incy, a, lda);
+}
+

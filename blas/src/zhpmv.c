355b4cfabfd18658c4c76a8e41b1e7501e22cedb
Ruben Hensen <ruben.hensen@protonmail.com>
2024-04-03 17:32:47 +0200

    Add a lot of non-working z functions and half sgbmv

---
+++ blas/src/zhpmv.c
@@ -0,0 +1,10 @@
+#include <cblas.h>
+
+void SACzhpmv(const char uplo, const int n, const void *alpha, const void *ap, const void *x, const int incx, const void *beta, void *y, const int incy)
+{
+    if (uplo == 'u' || uplo == 'U') {
+        cblas_zhpmv(CblasRowMajor, 121, n, alpha, ap, x, incx, beta, y, incy);
+    } else {
+        cblas_zhpmv(CblasRowMajor, 122, n, alpha, ap, x, incx, beta, y, incy);
+    }
+}
---
+++ blas/src/zhpr2.c
@@ -0,0 +1,10 @@
+#include <cblas.h>
+
+void SACzhpr2(const char uplo, const int n, const void *alpha, const void *x, const int incx, const void *y, const int incy, void *ap)
+{
+    if (uplo == 'u' || uplo == 'U') {
+        cblas_zhpr2(CblasRowMajor, 121, n, alpha, x, incx, y, incy, ap);
+    } else {
+        cblas_zhpr2(CblasRowMajor, 122, n, alpha, x, incx, y, incy, ap);
+    }
+}
